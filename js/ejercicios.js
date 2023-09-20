a8b57004edf6741b20e879a4df5a9eaf399de9b3
TIAGO SHEBAR <47699554@ort>
2023-09-20 09:23:14 -0300

    Faltan un par de cosas

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -1,6 +1,10 @@
+//Falta en todos hacer lo del DOM
+
+
 //1
 function CalculadoraDeEdad(fecha){
-    const hoy = Date.Today();
+    //no funciona
+    const hoy = new Date();
     const nacimiento = new Date(fecha);
     let edad = hoy.getFullYear() - nacimiento.getFullYear();
     const mes = hoy.getMonth() - nacimiento.getMonth();
@@ -17,7 +21,7 @@ function CalculadoraDeEdad(fecha){
 function TirandoFruta(){
     const frutas = ["mandarina", "sandia", "naranja", "pomelo", "melon", "uva", "frutilla", "arandanos", "manzana", "ciruela"];
     let i = 0;
-    const fruta = prompt("Ingrese una fruta: ").ToLowerCase();
+    const fruta = prompt("Ingrese una fruta: ").toLowerCase();
     while(fruta != frutas[i] && i < frutas.length){
         i++;
     };
@@ -31,17 +35,21 @@ function TirandoFruta(){
 
 //3
 function ComparandoDatosYTipos(){
+    //falta el D
+
     //A
     console.log(10 == '10');
     
     //B
     console.log(10 === '10');
+    //La diferencia que encontramos es que con el triple igual compara no solo el valor, sino también el tipo de dato, y por lo tanto va a dar false.
 
     //C
     console.log(typeof 10.6);
 
     //D
-
+    console.log(true==1);
+    //Es cierto, y de esto podemos interpretar que false == 0, por que compara un valor y no el tipo de dato. O sea que lo que interpreta como lenguaje binario.
 }
 
 //4
@@ -64,33 +72,34 @@ function DobleDeElementos(numeros){
     let doble;
     for(let i = 0; i < numeros.length; i++){
         doble.push(numeros[i]*2);
+        console.log(doble[i]);
     }
 }
 
 //6
 function TriánguloDeAsteriscos(){
     //A
-    for(let i = 1; i <= 5; i++){
-        console.log("*" * i);
+    const constante1 = 5;
+    for(let i = 1; i <= constante1; i++){
+        console.log("*".repeat(i));
     }
 
     //B
-    let j = 4;
-    for(let i = 1; i <= 9; i += 3){
-        if(i == 4){
-            i--;
-        }
-        console.log("-" * j + "*" * i + "-" * j);
-        j--;
+    const constante2 = 5;
+    let j = 1;
+    let h;
+    for(let i = 0; i < constante2; i++){
+        h = constante2*2-j;
+        console.log("-".repeat(h/2)+ "*".repeat(j) + "-".repeat(h/2));
+        j+=2;
     }
 }
 
 //7
 function NombresConA(){
     const nombres = prompt("Ingrese al menos dos nombres separados por una coma: ");
-    for(let i = 0; i<nombres.length; i++){
-        
-    }
+    const nombresConA = nombres.split(',').filter(nombre=>nombre[0] === 'A');
+    resultado.innerHTML = nombresConA;
 }
 
 //8
@@ -103,17 +112,47 @@ function ReemplazarPalabras(){
 }
 
 //9
-function CortarTexto(){
-
+function CortarTexto()
+{
+    const cadena = prompt("Ingrese una cadena de texto: ");
+    const num = prompt("¿En que posicion quiere cortar la cadena?: ");  
+    console.log(cadena.slice(num));
 }
 
 //10
-function StringConSeparador(){
-
+function StringConSeparador()
+{
+    const cadena = prompt("Ingrese una cadena de texto donde cada string este separado por una (,): ");
+    const cadena2 = cadena.replaceAll(",","-");
+    console.log(cadena2);
 }
 
 //11
 function CalculadoraDeRecaudacion(){
-    const objetosPerdidos = prompt("Ingrese una lista de objetos pedidos junto con sus respectivos totales, separados por coma: ");
-    
+    //no funciona
+    const pedidos = prompt("Ingrese una lista de objetos pedidos junto con sus respectivos totales, separados por coma (ej --> Pizza:15.50, Hamburguesa:8.75, Bebidas:4.25): ");
+    let antes = 0;
+    let suma = 0;
+
+    let resultado=pedidos.split(',');
+    for(let i=0;i<resultado;i++){
+        if(a){
+            resultado.slice(resultado[i]);
+        }
+    }
+    /*for(let i = 0; i<pedidos.length; i++){
+        if(pedidos[i] == ':'){
+            pedidos.slice(antes,pedidos[i].indexOf(':'));
+            antes = pedidos[i].indexOf(':');
+        }
+    }
+    antes = 0;
+    for(let i = 0; i<pedidos.length; i++){
+        if(pedidos[i] == ','){
+            suma += pedidos.indexOf(0,',');
+            pedidos.slice(antes,pedidos[i].indexOf(','));
+            antes = pedidos[i].indexOf(',');
+        }
+    }*/
+    console.log(suma);
 }
